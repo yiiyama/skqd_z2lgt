3128003f07d28925775b86acb6e623687c7bf681
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2025-12-11 12:28:21 +0900

    made coefficients dynamic

--- lib/unitary_krylov.py
+++ lib/unitary_krylov.py
@@ -9,38 +9,43 @@ from heavyhex_qft.triangular_z2 import TriangularZ2Lattice
 from skqd_z2lgt.jax_experimental_sparse_linalg import lobpcg_standard
 
 
-def make_hvec(hamiltonian):
+def make_hvec(hamiltonian, variable=False):
     nq = hamiltonian.num_qubits
-    minus_eplaq = next(coeff for pauli, coeff in zip(hamiltonian.paulis, hamiltonian.coeffs)
-                       if np.any(pauli.x))
-    minus_z_arr = np.array([-1., 1.], dtype=np.complex128)
-    minus_zz_arr = np.array([[-1., 1.], [1., -1.]], dtype=np.complex128)
+    z_arr = np.array([1., -1.], dtype=np.complex128)
+    zz_arr = np.array([[1., -1.], [-1., 1.]], dtype=np.complex128)
     shape_template = [1] * nq
 
-    @jax.jit
-    def hvec(state):
+    def hvec(state, plaquette_energy=None):
         shape_extra = state.shape[:-1]
         state = state.reshape(shape_extra + (2,) * nq)
         result = jnp.zeros_like(state)
-        for pauli in hamiltonian.paulis:
+        for pauli, coeff in zip(hamiltonian.paulis, hamiltonian.coeffs):
             zs = np.nonzero(pauli.z[::-1])[0].astype(np.int64)
             xs = np.nonzero(pauli.x[::-1])[0].astype(np.int64)
             if zs.shape[0] == 1:
                 shape = list(shape_template)
                 shape[zs[0]] = 2
-                result += jnp.reshape(minus_z_arr, shape) * state
+                result += jnp.reshape(coeff * z_arr, shape) * state
             elif zs.shape[0] == 2:
                 shape = list(shape_template)
                 shape[zs[0]] = 2
                 shape[zs[1]] = 2
-                result += jnp.reshape(minus_zz_arr, shape) * state
+                result += jnp.reshape(coeff * zz_arr, shape) * state
             else:
                 axis = len(shape_extra) + xs[0]
                 index = (slice(None),) * axis
                 index += (slice(None, None, -1),)
-                result += minus_eplaq * state[index]
+                if variable:
+                    result += -plaquette_energy * state[index]
+                else:
+                    result += coeff * state[index]
 
         return result.reshape(shape_extra + (2 ** nq,))
+
+    if variable:
+        hvec = jax.jit(hvec)
+    else:
+        hvec = partial(jax.jit, static_argnums=[1])(hvec)
 
     return hvec
 
@@ -48,31 +53,24 @@ def make_hvec(hamiltonian):
 def make_trotter_uvec(hamiltonian, delta_t):
     # exp(-iHΔt)x
     nq = hamiltonian.num_qubits
-    eplaq = next(-coeff for pauli, coeff in zip(hamiltonian.paulis, hamiltonian.coeffs)
-                 if np.any(pauli.x))
-    rz_arr = np.exp(np.array([1.j, -1.j]) * delta_t * 0.5)
-    rzz_arr = np.exp(np.array([[1.j, -1.j], [-1.j, 1.j]]) * delta_t * 0.5)
-    rx_mat = np.array(
-        [
-            [np.cos(eplaq * delta_t), 1.j * np.sin(eplaq * delta_t)],
-            [1.j * np.sin(eplaq * delta_t), np.cos(eplaq * delta_t)]
-        ]
-    )
+    z_arr = np.array([1., -1.], dtype=np.complex128)
+    zz_arr = np.array([[1., -1.], [-1., 1.]], dtype=np.complex128)
     shape_template = [1] * nq
 
     @jax.jit
     def apply_diag(result):
-        for pauli in hamiltonian.paulis:
+        """Apply exp(-i*0.5*Hdiag*Δt)."""
+        for pauli, coeff in zip(hamiltonian.paulis, hamiltonian.coeffs):
             zs = np.nonzero(pauli.z[::-1])[0].astype(np.int64)
             if zs.shape[0] == 1:
                 shape = list(shape_template)
                 shape[zs[0]] = 2
-                result *= jnp.reshape(rz_arr, shape)
+                result *= jnp.reshape(np.exp(-0.5j * coeff * delta_t * z_arr), shape)
             elif zs.shape[0] == 2:
                 shape = list(shape_template)
                 shape[zs[0]] = 2
                 shape[zs[1]] = 2
-                result *= jnp.reshape(rzz_arr, shape)
+                result *= jnp.reshape(np.exp(-0.5j * coeff * delta_t * zz_arr), shape)
         return result
 
     @jax.jit
@@ -80,9 +78,15 @@ def make_trotter_uvec(hamiltonian, delta_t):
         result = state.reshape((2,) * nq)
         result = apply_diag(result)
 
-        for pauli in hamiltonian.paulis:
+        for pauli, coeff in zip(hamiltonian.paulis, hamiltonian.coeffs):
             xs = np.nonzero(pauli.x[::-1])[0].astype(np.int64)
             if xs.shape[0] == 1:
+                rx_mat = np.array(
+                    [
+                        [np.cos(coeff * delta_t), -1.j * np.sin(coeff * delta_t)],
+                        [-1.j * np.sin(coeff * delta_t), np.cos(coeff * delta_t)]
+                    ]
+                )
                 result = jnp.moveaxis(jnp.tensordot(rx_mat, result, ([1], [xs[0]])), 0, xs[0])
 
         result = apply_diag(result)
@@ -99,8 +103,15 @@ def integrate(hvec, nplaq, tpoints, psi0=None):
     return odeint(lambda state, _: -1.j * hvec(state), psi0, tpoints, rtol=1.e-10, atol=1.e-10)
 
 
-@partial(jax.jit, static_argnums=[0, 1, 2, 3])
-def simulate(trotter_uvec, nplaq, krylov_dim, num_substeps=2, psi0=None):
+@partial(jax.jit, static_argnums=[0, 1, 2, 3, 4])
+def simulate(
+    trotter_uvec,
+    nplaq,
+    krylov_dim,
+    num_substeps=2,
+    dims=None,
+    psi0=None,
+):
     if psi0 is None:
         psi0 = jax.nn.one_hot(0, 2 ** nplaq, dtype=np.complex128)
 
@@ -111,7 +122,17 @@ def simulate(trotter_uvec, nplaq, krylov_dim, num_substeps=2, psi0=None):
         return y, y
 
     psis = jax.lax.scan(f, psi0, length=krylov_dim)[1]
-    return jnp.concatenate([psi0[None, :], psis], axis=0)
+    if dims is None:
+        return jnp.concatenate([psi0[None, :], psis], axis=0)
+
+    dims = list(sorted(dims))
+    if min(dims) != 0:
+        indices = np.array(dims) - 1
+        return psis[indices]
+
+    psi_list = [psi0[None, :] if dim == 0 else psis[dim - 1] for dim in dims]
+    return jnp.concatenate(psi_list, axis=0)
+
 
 
 @partial(jax.jit, static_argnums=[0, 1, 2, 3])
