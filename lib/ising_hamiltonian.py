baac57fa4813d0e1de21f6ab7b095aa67fce5162
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2026-03-17 16:38:29 +0900

    Factored out Hamiltonian maker

---
+++ lib/ising_hamiltonian.py
@@ -0,0 +1,90 @@
+"""Yet another Hv-op and Uv-op maker. Used in ground_4x8.py."""
+import numpy as np
+import jax
+import jax.numpy as jnp
+
+
+def make_apply_h(hamiltonian, sh_qubit=None):
+    """Apply-H highly optimized for the Ising Hamiltonian of the 2D Z2 LGT.
+    
+    Supply a NamedSharding compatible with a shape (2,) * nq array if sharding the input vector.
+    """
+    z_coeffs = {}
+    zz_coeffs = {}
+    x_coeff = 0.
+
+    # We know coeffs are all real
+    for pauli, coeff in zip(hamiltonian.paulis, hamiltonian.coeffs.real):
+        zs = np.nonzero(pauli.z[::-1])[0].astype(np.int64)
+        if zs.shape[0] == 1:
+            if zs[0] in z_coeffs:
+                z_coeffs[zs[0]] += coeff
+            else:
+                z_coeffs[zs[0]] = coeff
+        elif zs.shape[0] == 2:
+            key = (zs[0], zs[1])
+            if key in zz_coeffs:
+                zz_coeffs[key] += coeff
+            else:
+                zz_coeffs[key] = coeff
+        else:
+            # We know there are only ZZ, Z, and X terms, and there is one X term per qubit
+            x_coeff = coeff
+
+    nq = hamiltonian.num_qubits
+
+    def make_apply_z(axis):
+        def apply_z(vec, coeff):
+            axes = list(range(nq))
+            axes.remove(axis)
+            return jnp.expand_dims(jnp.array([coeff, -coeff]), tuple(axes)) * vec
+
+        return apply_z
+
+    def make_apply_zz(axis1, axis2):
+        def apply_zz(vec, coeff):
+            axes = list(range(nq))
+            axes.remove(axis1)
+            axes.remove(axis2)
+            return jnp.expand_dims(jnp.array([[coeff, -coeff], [-coeff, coeff]]), tuple(axes)) * vec
+
+        return apply_zz
+
+    def make_apply_x(axis):
+        def apply_x(vec):
+            return jnp.flip(vec, axis=axis)
+
+        return apply_x
+
+    z_fns = [make_apply_z(ax) for ax in z_coeffs.keys()]
+    z_coeffs = jnp.array(list(z_coeffs.values()))
+    zz_fns = [make_apply_zz(*axs) for axs in zz_coeffs.keys()]
+    zz_coeffs = jnp.array(list(zz_coeffs.values()))
+    x_fns = [make_apply_x(ax) for ax in range(nq)]
+
+    def z_body(iop, val):
+        result, vec, coeffs = val
+        result += jax.lax.switch(iop, z_fns, vec, coeffs[iop])
+        return (result, vec, coeffs)
+
+    def zz_body(iop, val):
+        result, vec, coeffs = val
+        result += jax.lax.switch(iop, zz_fns, vec, coeffs[iop])
+        return (result, vec, coeffs)
+
+    def x_body(iop, val):
+        result, vec = val
+        result += jax.lax.switch(iop, x_fns, vec)
+        return (result, vec)
+
+    @jax.jit
+    def apply_h(vec):
+        vec = vec.reshape((2,) * nq, out_sharding=sh_qubit)
+        result = jnp.zeros_like(vec)
+        result = jax.lax.fori_loop(0, nq, x_body, (result, vec))[0]
+        result *= x_coeff
+        result = jax.lax.fori_loop(0, z_coeffs.shape[0], z_body, (result, vec, z_coeffs))[0]
+        result = jax.lax.fori_loop(0, zz_coeffs.shape[0], zz_body, (result, vec, zz_coeffs))[0]
+        return result.reshape(-1, out_sharding=jax.typeof(vec).sharding)
+
+    return apply_h
--- scripts/ground_4x8.py
+++ scripts/ground_4x8.py
@@ -1,18 +1,21 @@
-import os
+import sys
+from pathlib import Path
 import logging
 import numpy as np
 import h5py
 import jax
-import jax.numpy as jnp
 from jax.sharding import PartitionSpec, AxisType, NamedSharding
 from heavyhex_qft.triangular_z2 import TriangularZ2Lattice
 from skqd_z2lgt.ground_locg import ground_locg
+sys.path.append(str(Path(__file__).parents[1] / 'lib'))
+from ising_hamiltonian import make_apply_h
 
 jax.config.update('jax_enable_x64', True)
 logging.basicConfig(level=logging.INFO)
 LOG = logging.getLogger()
 
-mesh_qubit = jax.make_mesh((2, 2, 2), ('X', 'Y', 'Z'), axis_types=(AxisType.Explicit, AxisType.Explicit, AxisType.Explicit))
+mesh_qubit = jax.make_mesh((2, 2, 2), ('X', 'Y', 'Z'),
+                           axis_types=(AxisType.Explicit, AxisType.Explicit, AxisType.Explicit))
 sh_single = NamedSharding(mesh_qubit, PartitionSpec(('X', 'Y', 'Z')))
 sh_qubit = NamedSharding(mesh_qubit, PartitionSpec('X', 'Y', 'Z'))
 
@@ -24,86 +27,6 @@ base_link_state[::-1][[26, 28, 31, 34]] = 1
 dual_lattice = lattice.plaquette_dual(base_link_state)
 nplaq = lattice.num_plaquettes
 hamiltonian = dual_lattice.make_hamiltonian(0.8)
-
-def make_apply_z(nq, axis):
-    def apply_z(vec, coeff):
-        axes = list(range(nq))
-        axes.remove(axis)
-        return jnp.expand_dims(jnp.array([coeff, -coeff]), tuple(axes)) * vec
-
-    return apply_z
-
-def make_apply_zz(nq, axis1, axis2):
-    def apply_zz(vec, coeff):
-        axes = list(range(nq))
-        axes.remove(axis1)
-        axes.remove(axis2)
-        return jnp.expand_dims(jnp.array([[coeff, -coeff], [-coeff, coeff]]), tuple(axes)) * vec
-
-    return apply_zz
-
-def make_apply_x(axis):
-    def apply_x(vec):
-        return jnp.flip(vec, axis=axis)
-
-    return apply_x
-
-def make_apply_h(hamiltonian):
-    z_coeffs = {}
-    zz_coeffs = {}
-    x_coeff = 0.
-
-    for pauli, coeff in zip(hamiltonian.paulis, hamiltonian.coeffs.real):
-        zs = np.nonzero(pauli.z[::-1])[0].astype(np.int64)
-        xs = np.nonzero(pauli.x[::-1])[0].astype(np.int64)
-        if zs.shape[0] == 1:
-            if zs[0] in z_coeffs:
-                z_coeffs[zs[0]] += coeff
-            else:
-                z_coeffs[zs[0]] = coeff
-        elif zs.shape[0] == 2:
-            key = (zs[0], zs[1])
-            if key in zz_coeffs:
-                zz_coeffs[key] += coeff
-            else:
-                zz_coeffs[key] = coeff
-        else:
-            x_coeff = coeff
-
-    nq = hamiltonian.num_qubits
-    z_fns = [make_apply_z(nq, ax) for ax in z_coeffs.keys()]
-    z_coeffs = jnp.array(list(z_coeffs.values()))
-    zz_fns = [make_apply_zz(nq, *axs) for axs in zz_coeffs.keys()]
-    zz_coeffs = jnp.array(list(zz_coeffs.values()))
-    x_fns = [make_apply_x(ax) for ax in range(nq)]
-
-    def z_body(iop, val):
-        result, vec, coeffs = val
-        result += jax.lax.switch(iop, z_fns, vec, coeffs[iop])
-        return (result, vec, coeffs)
-
-    def zz_body(iop, val):
-        result, vec, coeffs = val
-        result += jax.lax.switch(iop, zz_fns, vec, coeffs[iop])
-        return (result, vec, coeffs)
-
-    def x_body(iop, val):
-        result, vec = val
-        result += jax.lax.switch(iop, x_fns, vec)
-        return (result, vec)
-
-
-    @jax.jit
-    def apply_h(vec):
-        vec = vec.reshape((2,) * nplaq, out_sharding=sh_qubit)
-        result = jnp.zeros_like(vec)
-        result = jax.lax.fori_loop(0, nq, x_body, (result, vec))[0]
-        result *= x_coeff
-        result = jax.lax.fori_loop(0, z_coeffs.shape[0], z_body, (result, vec, z_coeffs))[0]
-        result = jax.lax.fori_loop(0, zz_coeffs.shape[0], zz_body, (result, vec, zz_coeffs))[0]
-        return result.reshape(-1, out_sharding=sh_single)
-
-    return apply_h
 
 apply_h = make_apply_h(hamiltonian)
 
