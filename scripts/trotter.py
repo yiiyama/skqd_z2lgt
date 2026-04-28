524b68790df82459631777ffc1c3edef88c2c8ab
Yutaro Iiyama <yutaro.iiyama@cern.ch>
2026-04-29 07:28:44 +0900

    trotter.py as a more full-fledged part of the toolchain

--- scripts/trotter.py
+++ scripts/trotter.py
@@ -10,6 +10,9 @@ import jax
 import jax.numpy as jnp
 from jax.sharding import PartitionSpec, AxisType, NamedSharding
 from heavyhex_qft.triangular_z2 import TriangularZ2Lattice
+from skqd_z2lgt.mwpm import minimum_weight_link_state
+from skqd_z2lgt.parameters import Parameters
+from skqd_z2lgt.tasks.open_output import open_output
 sys.path.append(str(Path(__file__).parents[1] / 'lib'))
 from ising_hamiltonian import make_apply_u
 
@@ -17,13 +20,10 @@ from ising_hamiltonian import make_apply_u
 if __name__ == '__main__':
     from argparse import ArgumentParser
     parser = ArgumentParser()
-    parser.add_argument('nrow', type=int)
-    parser.add_argument('ncol', type=int)
-    parser.add_argument('plaquette_energy', type=float)
-    parser.add_argument('dt', type=float)
-    parser.add_argument('nsteps', type=int)
+    parser.add_argument('pkgpath')
+    parser.add_argument('--nsteps', type=int, required=True)
+    parser.add_argument('--idt', type=int, default=0)
     parser.add_argument('--gpus')
-    parser.add_argument('--out', default='.')
     parser.add_argument('--xprof')
     parser.add_argument('--localmpi', action='store_true')
     options = parser.parse_args()
@@ -32,22 +32,13 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     LOG = logging.getLogger()
 
-    lattice = TriangularZ2Lattice((options.nrow, options.ncol))
-    base_link_state = np.zeros(lattice.num_links, dtype=np.uint8)
-
-    nsites = (options.ncol - 1) // 2 + 1 + (options.nrow // 2 + 1) % 2
-    vfirst = (lattice.num_vertices - nsites) // 2
-    charged_vertices = [vfirst, vfirst + nsites - 1]
-    links = []
-    for ivtx in range(*charged_vertices):
-        links.append(lattice.graph.edge_indices_from_endpoints(ivtx, ivtx + 1)[0])
-    base_link_state = np.zeros(lattice.num_links, dtype=np.uint8)
-    base_link_state[::-1][links] = 1
+    parameters = open_output(options.pkgpath)
+    lattice = TriangularZ2Lattice(parameters.lgt.lattice)
+    base_link_state = minimum_weight_link_state(parameters.lgt.charged_vertices, lattice)
     dual_lattice = lattice.plaquette_dual(base_link_state)
-    nplaq = lattice.num_plaquettes
-    hamiltonian = dual_lattice.make_hamiltonian(options.plaquette_energy)
-
+    hamiltonian = dual_lattice.make_hamiltonian(parameters.lgt.plaquette_energy)
     apply_u = make_apply_u(hamiltonian, axis_type=AxisType.Explicit)
+    dt = parameters.circuit.dts[options.idt]
 
     sharding = None
     if options.gpus:
@@ -75,9 +66,10 @@ if __name__ == '__main__':
 
     @jax.jit
     def run():
-        vec = jax.device_put(jax.nn.one_hot(0, 2 ** nplaq, dtype=np.complex128), sharding)
+        vec = jax.device_put(jax.nn.one_hot(0, 2 ** lattice.num_plaquettes, dtype=np.complex128),
+                             sharding)
         for _ in range(options.nsteps):
-            vec = apply_u(vec, options.dt)
+            vec = apply_u(vec, dt)
         probs = jnp.square(jnp.abs(vec))
         return probs
 
@@ -87,10 +79,9 @@ if __name__ == '__main__':
     else:
         probs = run()
 
-    filename = f'trotter_{options.nrow}x{options.ncol}_l{options.plaquette_energy:.2f}'
-    filename += f'_dt{options.dt:.3f}_s{options.nsteps}.h5'
+    path = Path(options.pkgpath) / f'trotter_idt{options.idt}_s{options.nsteps}.h5'
     if proc_id == 0:
-        with h5py.File(str(Path(options.out) / filename), 'w', libver='latest') as out:
+        with h5py.File(str(path), 'w', libver='latest') as out:
             out.create_dataset('probs', shape=probs.shape[0], dtype=np.float64)
     else:
         MPI.COMM_WORLD.recv(source=proc_id - 1, tag=11)
@@ -98,7 +89,7 @@ if __name__ == '__main__':
     LOG.info('Writing from process %d on indices %s', proc_id,
              list(shard.index for shard in probs.addressable_shards))
 
-    with h5py.File(str(Path(options.out) / filename), 'a', libver='latest') as out:
+    with h5py.File(str(path), 'a', libver='latest') as out:
         for shard in probs.addressable_shards:
             out['probs'][shard.index] = shard.data
 
